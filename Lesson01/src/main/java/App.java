e6985b223437cd81a294af87ff7cbc0807c28a2f
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2025-02-21 18:48:20 +0000

    Lesson 01: JVM Introduction

---
+++ Lesson01/src/main/java/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        String a = "Sport ";
+        String b = "Lisboa ";
+        String c = "e ";
+        String d = "Benfica!!!";
+        String e = a + b + c + d;
+        System.out.println(e);
+
+        C.classMethod();
+
+        C c1 = new C();
+        c1.instanceMethod();
+        c1.classMethod();
+
+
+    }
+}
---
+++ Lesson01/src/main/java/Slb.java
@@ -0,0 +1,19 @@
+class A {
+
+}
+
+class B {
+
+}
+
+class C {
+    public C() {
+    }
+    public static void classMethod(){
+        System.out.println("static method");
+    }
+
+    public void instanceMethod(){
+        System.out.println("instance method");
+    }
+}
